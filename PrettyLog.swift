f2b80eb99baa06195b1c7888cdd061c2065bad0e
Aladdin.Z <aladdin@umeng.com>
2014-09-26 11:47:39 +0800

    Update PrettyLog.swift

--- PrettyLog.swift
+++ PrettyLog.swift
@@ -8,11 +8,24 @@
 
 import Foundation
 
+var g_PLlastFunction:String!=""
+var g_PLlastFile:String!=""
+var g_PLlastLine:String!=""
 func prettyLog(message: String,
     function: String = __FUNCTION__,
     file: String = __FILE__,
     line: Int = __LINE__) {
-        
-        println( "---> \(function) @{\(file):\(line)} => \(message)")
-        
+        var filenameStr = file.lastPathComponent
+        var comparedlineStr = "@{\(function):\(line)}"
+        var fileStr = "\(file)"
+        if (g_PLlastLine==comparedlineStr){
+            println( "\(filenameStr):\(line)>\t \(message)")
+        }else if (g_PLlastFile==file) {
+            g_PLlastLine = "@{\(function):\(line)}"
+            println( "\n-=>\t\(function) \n\(filenameStr):\(line)>\t \(message)")
+        }else {
+            g_PLlastFile = fileStr
+            println( "\n------------------------------------------------------------")
+            println( "-->\t\(fileStr)\n-=>\t\(function) \n\(filenameStr):\(line)>\t \(message)")
+        }
 }
